b80f70ba7ce34bbf1d04ac286ae2a89fbaace56f
TesterTheCoded <weronika.krzynowek@thecoded.co.uk>
2018-06-06 14:20:38 +0200

    TRAIN-175

---
+++ Connection.php
@@ -0,0 +1,32 @@
+<?php
+
+class Connetcion
+{
+    private static $db;
+    private function __construct()
+    {
+        $config = require_once 'config.php';
+
+        $host = $config['host'];
+        $database = $config['database'];
+        $user = $config['user'];
+        $password = $config['password'];
+
+        try {
+            self::$db = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password, [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+        } catch (PDOException $error) {
+            exit("database error");
+        }
+    }
+
+    public static function getConn()
+    {
+        if(self::$db === null) {
+            new self();
+            return self::$db;
+        }
+    }
+}
